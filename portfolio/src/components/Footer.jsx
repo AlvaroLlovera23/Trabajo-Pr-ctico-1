b16857af041ebd97246c7a90103ab35d3a7f29a7
alvarollovera23 <alvarollovera14@gmail.com>
2023-05-11 14:18:55 -0300

    1er tp

---
+++ portfolio/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {AiFillGithub} from 'react-icons/ai'
+import '../styles/Footer.css'
+
+const Footer = () => {
+  return (
+    <div>
+      <AiFillGithub/>
+      <h3 id= "Gh">Mi github</h3>
+    </div>
+  )
+}
+
+export default Footer
---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Header () {
+
+return (
+  <div className="div">
+    <h1 className="h1">
+        Alvaro Llovera
+    </h1>
+    <img src="https://e.rpp-noticias.io/large/2022/02/19/191919_1219777.jpg" alt="" />
+  </div>
+);
+}
+
+export default Header
